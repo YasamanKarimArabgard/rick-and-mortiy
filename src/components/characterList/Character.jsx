a161d1a7a0c87c3004d78a197892d5c3e5354036
jasminearabgard <yasaman.arabgard@outlook.com>
2023-09-22 18:04:56 +0330

    seperate char com in 2 comp

--- src/components/characterList/Character.jsx
+++ src/components/characterList/Character.jsx
@@ -1,31 +1,20 @@
 import { EyeIcon, EyeSlashIcon } from '@heroicons/react/24/outline';
+import MYChar from './Character';
 
-const Character = ({ characters, selectId, selectedChar}) => {
+const Character = ({ characters, selectId, selectedChar }) => {
     return (
         <>
             {
                 characters.map(char => (
-                    <div key={char.id} className='flex bg-slate-800 rounded-lg p-2'>
-                        <div>
-                            <img src={char.image} className="w-10 h-10 rounded-md" alt={char.name} />
-                        </div>
-                        <div className='flex justify-between w-full px-2 items-center'>
-                            <div className='flex flex-col'>
-                                <span className='text-slate-200 font-bold text-md'>{char.name}</span>
-                                <div className="flex gap-x-2 items-center">
-                                    <span className={`w-2 h-2 rounded-full ${char.status === 'Alive' ? 'bg-green-600' : 'bg-red-600'}`}></span>
-                                    <span className='text-slate-200 text-sm'>{char.status} - {char.species}</span>
-                                </div>
-                            </div>
-                            <button onClick={() => selectId(char.id)} className='cursor-pointer' >
-                                {
-                                    selectedChar === char.id ?
-                                        <EyeSlashIcon className='w-7 h-7 text-red-700 mr-2' /> :
-                                        <EyeIcon className='w-7 h-7 text-red-700 mr-2' />
-                                }
-                            </button>
-                        </div>
-                    </div>
+                    <MyChar char={char} selectId={selectId} selectedChar={selectedChar} key={char.id}>
+                        <button onClick={() => selectId(char.id)} className='cursor-pointer' >
+                            {
+                                selectedChar === char.id ?
+                                    <EyeSlashIcon className='w-7 h-7 text-red-700 mr-2' /> :
+                                    <EyeIcon className='w-7 h-7 text-red-700 mr-2' />
+                            }
+                        </button>
+                    </MyChar>
                 ))
             }
         </>
@@ -33,3 +22,23 @@ const Character = ({ characters, selectId, selectedChar}) => {
 };
 
 export default Character;
+
+export const MyChar = ({ char, children }) => {
+    return (
+        <div className='flex bg-slate-800 rounded-lg p-2'>
+            <div>
+                <img src={char.image} className="w-10 h-10 rounded-md" alt={char.name} />
+            </div>
+            <div className='flex justify-between w-full px-2 items-center'>
+                <div className='flex flex-col'>
+                    <span className='text-slate-200 font-bold text-md'>{char.name}</span>
+                    <div className="flex gap-x-2 items-center">
+                        <span className={`w-2 h-2 rounded-full ${char.status === 'Alive' ? 'bg-green-600' : 'bg-red-600'}`}></span>
+                        <span className='text-slate-200 text-sm'>{char.status} - {char.species}</span>
+                    </div>
+                </div>
+                {children}
+            </div>
+        </div>
+    )
+}
